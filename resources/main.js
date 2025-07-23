b6ad146cbfc350477082c301d725a8e4a0c86644
Bomy Rhee <50459262+20201141@users.noreply.github.com>
2025-07-23 00:25:29 -0400

    reupload resource folder

---
+++ resources/main.js
@@ -0,0 +1,21 @@
+/*For menubar dropdown */
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdownFunc() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.fa.fa-caret-down')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
